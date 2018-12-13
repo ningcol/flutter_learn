72d1855677b5b42880a2d2ea9a0a6a1c788dd580
ningcol <me@ningcol.com>
2018-12-13 20:42:14 +0800

    搜索框跳转

--- lib/view/searchTextField.dart
+++ lib/view/searchTextField.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:douabn/controller/movie.dart';
 
 class SearchTextField extends StatefulWidget {
   @override
@@ -16,18 +17,43 @@ class _SearchTextFieldState extends State<SearchTextField> {
         child: new MaterialButton(
           padding: const EdgeInsets.only(left: 10),
           highlightColor: null,
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context).pushReplacement(new MaterialPageRoute(
+                builder: (context) {
+                  return new SearchPage();
+                }
+            ));
+          },
           child: new Row(
             children: <Widget>[
-              new Icon(
-                Icons.search,
-                color: Colors.grey,
-                size: 24.0
+              new Row(
+                children: <Widget>[
+                  new Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                      size: 24.0
+                  ),
+                ],
               ),
-              new Text(
-                "海王",
-                style: new TextStyle(color: Colors.grey),
+              new Expanded(
+                child: new Text(
+                    "海王",
+                    style: new TextStyle(color: Colors.grey),
+                  ),
               ),
+              new Row(
+                children: <Widget>[
+                  new IconButton(
+                    icon: new Icon(Icons.filter_center_focus),
+                    color: Colors.grey,
+                    padding: const EdgeInsets.all(0),
+                    onPressed: (){
+                      print("11111");
+                    },
+                  ),
+                  new Padding(padding: const EdgeInsets.only(right: 5)),
+                ],
+              )
             ],
           ),
         ),
@@ -35,3 +61,85 @@ class _SearchTextFieldState extends State<SearchTextField> {
     );
   }
 }
+
+
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+
+}
+
+class _SearchPageState extends State<SearchPage> {
+
+
+  Widget inputTextFiled(){
+    return  new Row(
+      children: <Widget>[
+        new Expanded(
+          child: new Container(
+              height: 36,
+              padding: const EdgeInsets.only(left: 12),
+              child: new Material(
+                borderRadius: BorderRadius.circular(18.0),
+                color: Colors.white,
+                child: new Row(
+                  children: <Widget>[
+                    new Row(
+                      children: <Widget>[
+                        new Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                            size: 24.0
+                        ),
+                      ],
+                    ),
+                    new Expanded(
+                      child: new TextField(
+                        autofocus: true,
+                        decoration: new InputDecoration.collapsed(
+                          hintText: "你做的最浪漫的事是什么",
+                          hintStyle: new TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+        ),
+        new Row(
+          children: <Widget>[
+            new Container(
+              height: 44,
+              width: 60,
+              child: new FlatButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: new Text(
+                  '取消',
+                ),
+              )
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: inputTextFiled(),
+          titleSpacing: 0,  //标题间距
+        ),
+        body: new Text('adddd'),
+    );
+  }
+}
+
+
